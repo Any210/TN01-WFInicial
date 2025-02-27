b7abdb862ef7024c5db9a74e2e13327ea9e02ef9
kauanyvssousa <kauany.vssousa@senacsp.edu.br>
2025-02-27 20:07:23 -0300

    Corrigido form Ex 1

---
+++ WFInicial/FormEx1.cs
@@ -0,0 +1,37 @@
+namespace WFInicial
+{
+    public partial class FormEx1 : Form
+    {
+        public FormEx1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double nota1 = double.Parse(txtNota1.Text);
+            double nota2 = double.Parse(txtNota2.Text);
+
+            double media = (nota1 + nota2) / 2;
+
+            if (media >= 7)
+            {
+                MessageBox.Show("Ok", "Aprovado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (media < 5)
+            {
+                MessageBox.Show("Deu Ruim", "Reprovado", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Atenção", "Recuperação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void txtNota1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
