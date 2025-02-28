d1fa8f63486ce7db55dc4941fe1420394b6fa761
kauanyvssousa <kauany.vssousa@senacsp.edu.br>
2025-02-27 22:22:19 -0300

    feat: ex 3 corrigido no console

---
+++ CEx3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CEx3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o Primeiro Número: ");
+            int num1 = int.Parse(Console.ReadLine()!);
+            Console.Write("\nDigite o Segundo Número: ");
+            int num2 = int.Parse(Console.ReadLine()!);
+
+            int maior = Math.Max(num1, num2);
+
+            Console.WriteLine($"\nO número {maior} é o maior valor!");
+        }
+    }
+}
